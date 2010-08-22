31f1c2a9625a873740a793d2b9ddf705ed2e979d
c9s <cornelius.howl@gmail.com>
2010-08-23 03:02:01 +0800

    import Lang command.

---
+++ lib/App/Po/Command/Lang.pm
@@ -0,0 +1,26 @@
+package App::Po::Command::Lang;
+use warnings;
+use strict;
+use Cwd;
+use App::Po::Config;
+use App::Po::Logger;
+use File::Basename;
+use File::Path qw(mkpath);
+use File::Find::Rule;
+use base qw(App::Po::Command);
+
+=head1 Lang Command
+
+
+=cut
+
+sub run {
+	my ($class) = @_;
+	# create language file
+
+
+
+
+}
+
+1;
