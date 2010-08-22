07a51fd6a853a99957f9fd790b852c6b8286db15
c9s <cornelius.howl@gmail.com>
2010-08-23 03:50:28 +0800

    add aliases

--- lib/App/Po/Command.pm
+++ lib/App/Po/Command.pm
@@ -3,6 +3,15 @@ use warnings;
 use strict;
 use base qw(App::CLI App::CLI::Command);
 
+
+sub alias {
+    (
+        "s" => "server",
+        "p" => "parse",
+        "l" => "lang",
+    );
+}
+
 sub invoke {
     my ($pkg, $cmd, @args) = @_;
     local *ARGV = [$cmd, @args];
