c248f2c491ac3fb0f9eb22eb26af33802750a544
c9s <cornelius.howl@gmail.com>
2010-10-05 18:22:47 +0800

    Fix status command for --locale option

--- lib/App/I18N/Command/Status.pm
+++ lib/App/I18N/Command/Status.pm
@@ -39,7 +39,13 @@ sub run {
     my @pofiles = File::Find::Rule->file->name( "*.po" )->in( $podir );
     for my $pofile ( @pofiles ) {
         my $extract = Locale::Maketext::Extract->new;
-        my ($lang) = ($pofile =~ m{(\w+)\.po$} );   # get en_US or zh_TW ... etc
+
+        my $lang;
+        if( $self->{locale} ) {
+            ($lang) = ($pofile =~ m{(\w+)/LC_MESSAGES/} );   # get en_US or zh_TW ... etc
+        } else {
+            ($lang) = ($pofile =~ m{(\w+)\.po$} );   # get en_US or zh_TW ... etc
+        }
 
         $extract->read_po($pofile);
 
